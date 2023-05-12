19f10e40c56750069941a93e1ec6e41eb1673b4b
_USER_NAME <_EMAIL>
2023-05-12 13:28:01 +0530

    git init sh added

---
+++ git_init.sh
@@ -0,0 +1,5 @@
+export _USER_NAME="Sathishmahi"
+git config --global user.name _USER_NAME
+export _EMAIL="sathishmahi433@gmail.com"
+git config --global user.email _EMAIL
+echo "git Config  setup done"
